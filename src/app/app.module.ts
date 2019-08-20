9305ce8f08c4dd58a20cb37a6dd94ddec7ab4648
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2019-08-20 15:39:46 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutpageComponent
   ],
   imports: [
     BrowserModule,
